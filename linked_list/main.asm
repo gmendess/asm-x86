37d36b9a2ffa4448e2faa10b0c8c7f0dd7d93916
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-02-09 19:43:04 -0300

    Some changes and test

--- linked_list/main.asm
+++ linked_list/main.asm
@@ -1,17 +1,24 @@
+%include 'linked_list.asm'
+
 section .data
-  struc node
-    .value: resb 4
-    .next:  resb 4
-  endstruc
+  fmt: db "%d", 0xa, 0
 
 section .text
   extern printf
-  extern malloc
   global main
 
 main:
   push ebp
   mov ebp, esp
+
+  push 112233
+  call create_node
+  add esp, 4
+
+  push dword [eax + node.value]
+  push fmt
+  call printf
+  add esp, 8
 
   mov esp, ebp
   pop ebp
