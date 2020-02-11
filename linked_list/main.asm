ac50611ada4e2b5958819c0fbaa0f71da7b98616
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-02-10 21:19:38 -0300

    Testing the add_node function

--- linked_list/main.asm
+++ linked_list/main.asm
@@ -1,21 +1,52 @@
+; Assemble and link with:
+;   $ nasm -f elf32 main.asm
+;   $ gcc -m32 main.o -o main
+;   $ ./main
+
 %include 'linked_list.asm'
 
+section .bss
+  list_head: resb 4
+
 section .data
-  fmt: db "%d", 0xa, 0
+  fmt: db "Node value: %d", 0xa, 0
 
 section .text
   extern printf
   global main
 
 main:
+  nop             ; debug reasons
+
   push ebp
   mov ebp, esp
 
-  push 112233
-  call create_node
+  push 123456
+  call add_node
   add esp, 4
 
-  push dword [eax + node.value]
+  push 323232
+  call add_node
+  add esp, 4
+
+  push 999999
+  call add_node
+  add esp, 4
+
+  mov ebx, [list_head] ; points to first node
+  push dword [ebx + node.value]
+  push fmt
+  call printf
+  add esp, 8
+
+  mov ebx, [ebx + node.next] ; points to next node (2nd)
+  push dword [ebx + node.value]
+  push fmt
+  call printf
+  add esp, 8
+
+  mov ebx, [ebx + node.next] ; points to next node (3rd)
+  push dword [ebx + node.value]
   push fmt
   call printf
   add esp, 8
