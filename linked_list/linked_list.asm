800a0598ef248182f794cc1655c4f0a1f5a0bfbe
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-02-11 22:41:42 -0300

    Some changes in get_last_node. Now returns the node prior to the last

--- linked_list/linked_list.asm
+++ linked_list/linked_list.asm
@@ -7,14 +7,14 @@ endstruc
 
 ; Function that creates a struct node on heap and return a pointer to the region allocated
 ; @prototype:
-;   struct node* create_node(int value);
+;   struct node* create_node(int value)
 ;
 ; @return:
 ;   return the pointer in 'eax'
 create_node:
   push ebp        ; save ebp onto the stack
   mov ebp, esp    ; ebp points to the top of the stack, creating a stack frame
-  push ebx        ; save caller's ebx 
+  push ebx        ; save caller's ebx
 
   mov ebx, [ebp + 8] ; move to ebx the argument passed to the function
 
@@ -63,18 +63,22 @@ add_node:
   ret             ; return to caller
 
 ; Function that gets the last node in a list
-; @parameter:
+; @prototype:
 ;   struct node* get_last_node(void)
+;   struct node* -´
 ; @return:
-;   return the pointer in ebx
+;   return the last node in ebx
+;   return previews node in edx
 get_last_node:
   push ebp
   mov ebp, esp
 
+  xor edx, edx
 .iterate_list:
   mov ebx, [ebx]    ; ebx stores the address of the current node
   test dword [ebx + node.next], 0xFFFFFFFF ; check if the next node is a null pointer
   jz .return_now    ; if yes, we reached the last node, so it's .next member must point to eax
+  mov edx, ebx      ; save the node address in edx
   add ebx, 4        ; now ebx is the pointer to the next node
   jmp .iterate_list ; jump again to .iterate_list to check if the current node is the last one
 
